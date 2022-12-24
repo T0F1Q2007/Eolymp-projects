eb67c97f570fdd7a8c20aec25e8613f117514346
T0F1Q2007 <79853089+T0F1Q2007@users.noreply.github.com>
2022-12-24 15:18:32 +0400

    a1

---
+++ eolymp#8915.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int main(int argc,char *argv[])
+{
+    long n;
+    cin>>n;
+    for (int i=1;i<n;i+=2)
+    {
+        cout<<i<<" ";
+    }
+    return 0;
+}
